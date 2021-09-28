4deaa507896683759ff72ab314190860b4aa764a
ruslan.sibgatulin05@gmail.com <ruslan.sibgatulin05@gmail.com>
2021-09-29 00:39:13 +0600

    fixed recycler adapter and fixed recyclerview

--- app/src/main/java/com/example/dblearning/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/dblearning/fragments/list/ListFragment.kt
@@ -5,18 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dblearning.R
+import com.example.dblearning.data.UserViewModel
 import com.example.dblearning.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
     private lateinit var binding: FragmentListBinding
+    private lateinit var mUserViewModel: UserViewModel
+    private lateinit var adapter: ListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentListBinding.inflate(layoutInflater)
+
+        // Recyclerview
+        adapter = ListAdapter()
+        binding.recyclerview.adapter = adapter
+        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+
+        // UserViewModel
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        // readAllData возвращает queryset из объектов в виде List<User> и мы передаем его в адаптер
+        mUserViewModel.readAllData.observe(viewLifecycleOwner, Observer { user ->
+            adapter.setData(user)
+        })
 
         // так нас переключает на другой фрагмент
         binding.floatingActionButton.setOnClickListener {
