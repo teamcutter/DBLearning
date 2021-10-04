7461cb0f104d7e994e7c87cb32c6e924c890926b
ruslan.sibgatulin05@gmail.com <ruslan.sibgatulin05@gmail.com>
2021-10-04 15:19:59 +0600

    fixed bugs

--- app/src/main/java/com/example/dblearning/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/dblearning/fragments/update/UpdateFragment.kt
@@ -1,6 +1,6 @@
 package com.example.dblearning.fragments.update
 
-import android.app.AlertDialog
+import androidx.appcompat.app.AlertDialog
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextUtils
